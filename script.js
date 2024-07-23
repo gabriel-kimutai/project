a14ed4bbd10cfa724b9db06286daa90951a67a8a
gabriel-kimutai <gabrielkimutai997@gmail.com>
2024-07-23 16:05:27 +0300

    done

--- script.js
+++ script.js
@@ -1,49 +1,72 @@
-document.addEventListener('DOMContentLoaded',function(){
-    const search =document.getElementById('button');
-    const input=document.getElementById('input');
-    const resultContainer=document.getElementById('result-container');
-   // add event listener to search button.
-     button.addEventListener('click',function(){
-     // making request to local db.json
-     fetch("db.json")
-   .then(response=>{
-     // trows a new error when the request is not successful.
-     // if..else statement that throws an error is the request to server was not succeeful.
-     if(!response.ok){
-       throw new Error('Network response was not ok');
-     }
-     else{
-       return response.json();
-     }})
-     // this .then recives a parsed JSON data fromt the first .then.
-   .then(data =>{
-     // const data = response.json();
-     const recipes =data.recipes;
-     // the code lone below clear the result-contianer div that is to recieve the data from the server.
-     resultContainer.innerHTML='';
-   
-     // note- add code line to filter data to inpu text.
-     const filteredRecipes = recipes.filter(recipe => recipe.name.toLowerCase().includes(searchTerm));
-     // creating palceholder and new elemnts for displayed data and .
-     filteredRecipes.forEach(recipe => {
-       const recipeContainer = document.createElement('div');
-       recipeContainer.classList.add('recipe');
-       // creating placeholders for the actual data when displayed.
-       const recipeHtml=`
-       <h2>${recipe.name}</h2>
-       <ul>Ingredients: ${recipe.ingredients}</ul>
-           <p>Instructions: ${recipe.instructions}</p>
-             <p>PrepTime:${recipe.prepTime}</p>
-              <p>CookTime:${recipe.cookTime}</p>
-                <p>servings${recipe.servings}</p>`;
-         //  populates the created div with the HTML content genrated for each recipe item.
-           recipeContainer.innerHTML =recipeHtml;
-           resultContainer.appendChild(recipeContainer)
-     })
-   })
-     // catching an error and displaying the error
-   .catch(error);{
-     console.error('Error fetching and displaying data:',error);
-       }
+document.addEventListener('DOMContentLoaded', async function() {
+  // Assign DOM Elements to variables for later use
+  const inputBox = document.getElementById('input')
+  const searchButton = document.getElementById('button')
+  const resultContainer = document.getElementById('results-container')
+
+  let recipes = null
+  
+  // Try and fetch the recipes from `db.json` using fetch()
+  try {
+    const response = await fetch('db.json')
+    if (!response.ok) {
+      // Throw error incase the fetch failed
+      throw new Error("failed to fetch resource")
+    } else {
+      // Assign the response data to recipes variable after conversion to JSON
+      data = await response.json()
+      recipes = data.recipes
+    }
+  } catch (error) {
+    // Catch any errors encountered during the fetch
+    console.error("resource fetch error: ", error)
+  }
+
+  // Listen for `click` event on the button then perform recipe search
+  searchButton.addEventListener('click', function() {
+    // Get the search term from the input box
+    let searchTerm = inputBox.value
+
+    // Alert in case of empty search term
+    if (searchTerm === '') {
+      alert("Empty search term")
+    }
+
+    // Filter the recipes with respect to the search term provied by user
+    const filteredRecipes = recipes.filter((recipe) => {
+      return recipe.name.toLowerCase().includes(searchTerm);
+    })
+
+    // Alert if the search term is not found within the database
+    if (filteredRecipes.length === 0) {
+      alert('Recipe not found')
+      return
+    }
+
+    // Create DOM element in readiness to present to the user
+    filteredRecipes.forEach(recipe => {
+      const recipeContainer = document.createElement('div');
+      recipeContainer.classList.add('recipe');
+      const ingredientList = document.createElement('ul')
+      
+      recipe.ingredients.forEach((ingredient) => {
+        let ingredientListItem = document.createElement('li')
+        ingredientListItem.innerHTML = `${ingredient.name}, ${ingredient.quantity}`
+        ingredientList.appendChild(ingredientListItem)
       })
-   })
+      // Creating placeholders for the actual data when displayed.
+      const recipeHtml = `
+            <h2>${recipe.name}</h2>
+            <span>Ingredients:</span>
+            ${ingredientList.innerHTML}
+            <p id='instructions'>Instructions: ${recipe.instructions}</p>
+            <p>PrepTime:${recipe.prepTime}</p>
+            <p>CookTime:${recipe.cookTime}</p>
+            <p>servings${recipe.servings}</p>`;
+      //  populates the created div with the HTML content genrated for each recipe item.
+      recipeContainer.innerHTML = recipeHtml;
+      resultContainer.appendChild(recipeContainer)
+    })
+  })
+
+})
